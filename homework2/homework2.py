d86fd784302f0739acd52f616f096177fe750027
Emma Bennett-McConnell <emmabmc@berkeley.edu>
2026-09-20 12:03:35 -0700

    done

---
+++ homework2/homework2.py
@@ -0,0 +1,49 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+#Git is the software, GitHub is the website, and Git Bash is a terminal.
+# 2) What’s the difference between the terminal and the command line?
+#Terminal where the command line is found. The command line is where commands are typed.  
+# 3) How does Windows PowerShell differ from Git Bash?
+#Windows PowerShell is Window's command line language while GitBash is a linux command line that runs into Windows.
+# 4) What’s the difference between Anaconda, conda, and Python?
+#Anaconda is a collection of packages, conda manages Python enviroments and packages, and Python is a programming language. 
+# 5) What is VS Code? 
+#Workspace for programming and writing script. 
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+#Jupyter Notebook is a document where you can write script and see the result in the same space. Jupyter Lab is a collection of several notebooks along with terminals and text files. 
+# 7) What does ~/ mean?
+#Home directory.
+# 8) What’s the difference between an absolute path and a relative path?
+#An absolute path gives the full adress of a file from the root directory while a relative path gives the adress of a file based on your current folder location.
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+#Absolute path is /Users/emmabennett-mcconnell/python_decal_26/course_assignments/homework2 while relative path is course_assignments/homework2
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+# cd ..
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+#Deletes the directory 
+# 12) What do the following commands do?
+# git add puts a file in the "staging area" 
+# git commit saves the file 
+# git push uploads the file
+# 13) What's the difference between "git add ." and "git add <file>"?
+# git add . adds everything in the file/directory and git add <file> adds a specific file.  
+# 14) What do "git status" and "git log -1" do?
+#git status shows the state of your files (staging or if they have been pushed). git log -1 shows the most recent commit.
+# 15) What’s the difference between cloning a repository and pulling from it?
+#git clone downloads a repository while git pull downloads changes that have been made in a repository. 
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+#The most furstrating error I've had in this class was when I couldn't figure out how to add a screenshot into a directory. I was able to figure out how to do it by asking friends and looking it up. 
+# 17) What’s a question you still have? What’s something you’re confused about?
+#None for right now!
+# 18) Tell me a fun fact!
+#I'm from Virginia!
+# 19) Print your favorite math expression you've learned in Python so far. 
+# (Hint: Use print() and add a comment explaining what it does.)
+print(2 + 2) 
+#adds the two integers.
